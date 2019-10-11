8f0004d1d780ec12317c7c26efc96b02dd7125ea
=김지용 <znzld505@naver.com>
2019-10-11 09:01:34 +0900

    Updates

---
+++ precache-manifest.96f77a52e94d827c4d3c2c8511cee0ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca61a390ad488bb89f4a7c0933a9a73c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0549e420d67d8bdc2f23",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "0e7850f2eb633c5dbc87",
+    "url": "/static/css/main.4d6c74d4.chunk.css"
+  },
+  {
+    "revision": "0549e420d67d8bdc2f23",
+    "url": "/static/js/2.650056b0.chunk.js"
+  },
+  {
+    "revision": "0e7850f2eb633c5dbc87",
+    "url": "/static/js/main.e552fec4.chunk.js"
+  },
+  {
+    "revision": "c34fea74a0cd9707b32b",
+    "url": "/static/js/runtime-main.1aa95a99.js"
+  },
+  {
+    "revision": "c2629514c9505fdacb7a991ef1c3804b",
+    "url": "/static/media/mainVideo.c2629514.mp4"
+  }
+]);
