c5f81ad33609e01be4129241f39ee5ca95253711
kimjy0301 <znzld505@naver.com>
2019-10-15 22:02:56 +0900

    폰트 크기 변경

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -50,7 +50,7 @@ const Contact = ({ contactRef }) => {
                             lng={126.979246}
                         />
                     </GoogleMapReact>
-                    <div className="flex flex-col flex-wrap mt-6">
+                    <MailDIV className="flex flex-col flex-wrap mt-6">
                         <div data-aos="zoom-in" className="flex items-center flex-1 pt-1 pb-1">
                             <Icon xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path className="heroicon-ui" d="M13 20v-5h-2v5a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2v-7.59l-.3.3a1 1 0 1 1-1.4-1.42l9-9a1 1 0 0 1 1.4 0l9 9a1 1 0 0 1-1.4 1.42l-.3-.3V20a2 2 0 0 1-2 2h-3a2 2 0 0 1-2-2zm5 0v-9.59l-6-6-6 6V20h3v-5c0-1.1.9-2 2-2h2a2 2 0 0 1 2 2v5h3z" /></Icon>
                             <H1><span className="ml-2 mr-2">|</span><span>서울시 종로구 종로1길 50 더케이트윈타워 0호</span></H1>
@@ -63,12 +63,21 @@ const Contact = ({ contactRef }) => {
                             <Icon xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path className="heroicon-ui" d="M13.04 14.69l1.07-2.14a1 1 0 0 1 1.2-.5l6 2A1 1 0 0 1 22 15v5a2 2 0 0 1-2 2h-2A16 16 0 0 1 2 6V4c0-1.1.9-2 2-2h5a1 1 0 0 1 .95.68l2 6a1 1 0 0 1-.5 1.21L9.3 10.96a10.05 10.05 0 0 0 3.73 3.73zM8.28 4H4v2a14 14 0 0 0 14 14h2v-4.28l-4.5-1.5-1.12 2.26a1 1 0 0 1-1.3.46 12.04 12.04 0 0 1-6.02-6.01 1 1 0 0 1 .46-1.3l2.26-1.14L8.28 4z" /></Icon>
                             <H1><span className="ml-2 mr-2">|</span><span>010 - 1234 - 1234</span></H1>
                         </div>
-                    </div>
+                    </MailDIV>
                 </MapDIV>
             </DIV>
         </>
     )
 }
+
+const MailDIV = styled.div`
+    display:flex;
+    flex-direction:column;
+    flex-wrap:wrap;
+    margin-top:1.5rem;
+    width:max-content;
+`;
+
 const MarkerDIV = styled.div`
     color: #1a202c;
     width:max-content;
@@ -97,7 +106,7 @@ const Icon = styled.svg`
 `;
 
 const H1 = styled.h1`
-    font-size:0.7rem;
+    font-size:0.9rem;
     fill:#f7fafc;
     @media (min-width: 640px) {
         font-size:2rem;
